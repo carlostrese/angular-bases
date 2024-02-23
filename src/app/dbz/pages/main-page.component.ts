25290d0dd4f58ffd218b529de2f0e18906d8a0b9
eduardo dias <ediaz@intelisis.com.mx>
2024-02-23 12:56:15 -0600

    se termino la app

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DbzService } from '../services/dbz.service';
 import { v4 as uuid } from 'uuid';
+import { Character } from '../interfaces/character.interfaces';
 
 @Component({
   selector: 'app-dbz-main-pages',
@@ -8,7 +9,16 @@ import { v4 as uuid } from 'uuid';
 })
 
 export class MainPageComponent{
-  constructor(public dbzService:DbzService){
+  constructor(private dbzService:DbzService){
+  }
+  get characters():Character[]{
+    return [...this.dbzService.characters];
+  }
+  onDeleteCharacter(id:string):void{
+    this.dbzService.deleteCharacterById(id);
+  }
 
+  onNewCharacter(character: Character):void{
+    this.dbzService.addCharacter(character);
   }
 }
